16526855ee6b5cd79bef697c5e0dd23931e81c39
Muhammad Mohib <k250639@nu.edu.pk>
2025-10-28 07:10:23 -0700

    Create Question2.c

--- Question2.c
+++ Question2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int i, j, total;
+    int i, j;
     int yphotos[2][12]= {{12, 10, 15, 8, 5, 20, 25, 30, 10, 5, 8, 15}, {10, 12, 18, 9, 6, 22, 28, 35, 12, 7, 9, 16}};
     for(i=0; i<2; i++){
         for(j=0; j<12; j++){
